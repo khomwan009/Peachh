d5765e1b1f945b15a5a36972f0a8588969cea6ea
PeachJi <khomwan009@gmail.com>
2024-09-19 14:30:53 +0700

    first commit

---
+++ build/vesc/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/parallels/INC493/src/vesc-humble/install/vesc_msgs/share/vesc_msgs/package.sh"
+. "/home/parallels/INC493/src/vesc-humble/install/vesc_ackermann/share/vesc_ackermann/package.sh"
+. "/home/parallels/INC493/src/vesc-humble/install/vesc_driver/share/vesc_driver/package.sh"
---
+++ build/vesc_driver/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/parallels/INC493/src/vesc-humble/install/vesc_msgs/share/vesc_msgs/package.sh"
